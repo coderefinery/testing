df2ab420ff83a40575d96b21838b612edad3386a
jenswehner <jenswehner@gmail.com>
2021-06-29 10:41:51 +0200

    updated C++

--- content/code/cpp/even_time_difference.cpp
+++ content/code/cpp/even_time_difference.cpp
@@ -7,5 +7,5 @@ bool check_time_to_now_even(int seconds) {
 		throw std::runtime_error('received negative input');
 	}
   auto now = std::chrono::system_clock::now().time_since_epoch();
-    return std::chrono::duration_cast<std::chrono::seconds>(now).count()%seconds ==0;
+    return (std::chrono::duration_cast<std::chrono::seconds>(now).count()-seconds)%2 ==0;
 }
--- content/code/cpp/even_time_difference_sol.cpp
+++ content/code/cpp/even_time_difference_sol.cpp
@@ -1,9 +1,59 @@
-// Changing variables included from headers (monkey patching) is not possible in C++.
+// Changing variables included from headers (monkey patching) is not possible in C++. Instead we refactor the function to make it pure.
+//Another way would be to use templates for dependency injection
 #include <catch2/catch.hpp>
+
+//Making it pure
+bool check_time_to_now_even(int seconds, int current_time) {
+	if(seconds<0){
+		throw std::runtime_error('received negative input');
+	}
+    return (current_time-seconds)%2 ==0;
+}
+
+// Normal invocation
+auto now = std::chrono::system_clock::now().time_since_epoch();
+int current_time_seconds=std::chrono::duration_cast<std::chrono::seconds>(now).count();
+check_time_to_now_even(4,current_time_seconds);
 
 
 TEST_CASE("Check time difference", "[reactor_state]") {
-  REQUIRE(check_reactor_temperature(99) == ReactorState::FINE);
-  REQUIRE(check_reactor_temperature(100) == ReactorState::FINE);
-  REQUIRE(check_reactor_temperature(101) == ReactorState::CRITICAL);
+  REQUIRE(check_time_to_now_even(1,3) == true);
+  REQUIRE(check_time_to_now_even(4,5) == false);
+}
+
+
+//using templates
+
+template< class CLOCK>
+bool check_time_to_now_even(int seconds){
+  	if(seconds<0){
+		throw std::runtime_error('received negative input');
+	}
+  int current_time=CLOCK.getNowSeconds();
+  return (current_time-seconds)%2 ==0;
+}
+
+//Normal invocation
+class ChronoClock{
+
+int getNowSeconds(){
+  auto now = std::chrono::system_clock::now().time_since_epoch();
+ return std::chrono::duration_cast<std::chrono::seconds>(now).count();
+}
+
+};
+
+check_time_to_now_even<ChronoClock>(3);
+
+class TestClock{
+
+int getNowSeconds(){
+ return 3;
+}
+
+};
+
+TEST_CASE("Check time difference", "[reactor_state]") {
+  REQUIRE(check_time_to_now_even<TestClock>(3) == true);
+  REQUIRE(check_time_to_now_even<TestClock>(4) == false);
 }
